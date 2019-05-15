14d8ac0ccff236eff9d2a1d618700174c7a16a23
Philip <philiplofters@gmail.com>
2019-05-14 22:54:47 -0400

    Fixed more grammer and changed colors around

--- src/components/about.js
+++ src/components/about.js
@@ -6,9 +6,9 @@ const about = () => {
 			<div className="w-90 center tc mb5" >
 				<h2 className='tc f3 lh-copy ttu fw5  mb4' style={{fontFamily: 'Helvetica, system sans-serif'}}>About Me</h2>
 				<p className="mv0" style={{fontFamily: 'Georgia, system serif'}}>
-					I've always had an interest in web development, as someone who loves creation and design. 
-					I am a developer, with close to three years of experience of developing and deploying projects to production.
-					Going through that time of practicing and learning, has given me a flexible skillset. 
+					I've always had an interest in web development as someone who loves creation and design. 
+					I am a developer with close to three years of experience of developing and deploying projects to production.
+					Going through that time of practicing and learning has given me a flexible skillset. 
 					One where I can dance between the bounderies of being a Front-End Developer and being a Back-End Developer. 
 				</p>
 			</div>
--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -11,21 +11,21 @@ import {ReactComponent as MongoDB} from "./assets/mongodb.svg";
 
 const skills = () => {
 	return(
-		<div className="w-100 pt5 pb2" style={{backgroundColor: "#687864", color: "#f7f9fb", fontFamily: 'Helvetica, system sans-serif'}}>
+		<div className="w-100 pt5 pb2" style={{backgroundColor: "#f7f9fb", color: "#687864", fontFamily: 'Helvetica, system sans-serif'}}>
 			<h2 className='tc f3 lh-copy ttu fw5 mb4' style={{fontFamily: 'Helvetica, system sans-serif'}}>Skills</h2>
 			<h1 className="f4 mt4 mb0 tc pv3 ph5 fw2" style={{fontFamily: 'Helvetica, system sans-serif'}}>Web Technologies</h1>
 			<div className="flex justify-center">
 			  <div className="outline mw-20 w-20 w-10-l pa3 mr2">
-			    <Html style={{fill: "#f7f9fb"}} />
+			    <Html style={{fill: "#687864"}} />
 			  </div>
 			  <div className="outline mw-20 w-20 w-10-l pa3 mr2">
-			    <Css style={{fill: "#f7f9fb"}} />
+			    <Css style={{fill: "#687864"}} />
 			  </div>
 			  <div className="outline mw-20 w-20 w-10-l pa3 mr2">
-			    <JavaScript style={{fill: "#f7f9fb"}} />
+			    <JavaScript style={{fill: "#687864"}} />
 			  </div>
 			  <div className="outline mw-20 w-20 w-10-l pa3 mr2">
-			    <ReactLogo style={{fill: "#f7f9fb"}} />
+			    <ReactLogo style={{fill: "#687864"}} />
 			  </div>
 			</div>
 			<div className="flex justify-center">
@@ -45,30 +45,30 @@ const skills = () => {
 			<h1 className="f4 mt4 mb0 tc pv3 ph5 fw2" style={{fontFamily: 'Helvetica, system sans-serif'}}>Server & Databases</h1>
 			<div className="flex justify-center">
 			  <div className="outline mw-20 w-20 w-10-l pa3 mr2">
-			    <Node style={{fill: "#f7f9fb"}} />
+			    <Node style={{fill: "#687864"}} />
 			  </div>
 			  <div className="outline mw-20 w-20 w-10-l pa3 mr2">
-			    <Express style={{fill: "#f7f9fb"}} />
+			    <Express style={{fill: "#687864"}} />
 			  </div>
 			  <div className="outline mw-20 w-20 w-10-l pa3 mr2">
-			    <Postgres style={{fill: "#f7f9fb"}} />
+			    <Postgres style={{fill: "#687864"}} />
 			  </div>
 			  <div className="outline mw-20 w-20 w-10-l pa3 mr2">
-			    <MongoDB style={{fill: "#f7f9fb"}} />
+			    <MongoDB style={{fill: "#687864"}} />
 			  </div>		  
 			</div>
 			<div className="flex justify-center mb5">
 			  <div className="mw-20 w-20 w-10-l pa3 mr2">
-			    <h1 className="f5 lh-copy tc">Node</h1>
+			    <h1 className="tc f5 lh-copy tc">Node</h1>
 			  </div>
 			  <div className="mw-20 w-20 w-10-l pt3 mr2">
-			    <h1 className="f5 lh-copy tc">Express</h1>
+			    <h1 className="tc f5 lh-copy tc">Express</h1>
 			  </div>
 			  <div className="mw-20 w-20 w-10-l pt3 mr2">
-			    <h1 className="f5 lh-copy tc">Postgres</h1>
+			    <h1 className="tc f5 lh-copy tc">Postgres</h1>
 			  </div>
 			  <div className="mw-20 w-20 w-10-l pt3 mr2">
-			    <h1 className="f5 lh-copy tc">MongoDB</h1>
+			    <h1 className="tc f5 lh-copy tc">MongoDB</h1>
 			  </div>
 			  {/*
 			  <div className="mw-20 w-20 w-10-l pa3 mr2">
