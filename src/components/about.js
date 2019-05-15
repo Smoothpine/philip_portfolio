12711a3cb56296f78a8b2311dfd66e01df1a6ea9
Philip <philiplofters@gmail.com>
2019-05-15 04:31:03 -0400

    changed shade of blue on some elements

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ const about = () => {
 		<div className="w-100 pt5 pb2" style={{backgroundColor: "#f7f9fb", color: "#32718F"}}> 
 			<div className="w-90 center tc mb5" >
 				<h2 className='tc f3 lh-copy ttu fw5  mb4' style={{fontFamily: 'Helvetica, system sans-serif'}}>About Me</h2>
-				<p className="mv0" style={{fontFamily: 'Georgia, system serif'}}>
+				<p className="mv0" style={{fontFamily: 'Georgia, system serif', color: "#5085a5"}}>
 					I've always had an interest in web development as someone who loves creation and design. 
 					I am a developer with close to three years of experience of developing and deploying projects to production.
 					Going through that time of practicing and learning has given me a flexible skillset. 
