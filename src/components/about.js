21e0c71acdef14dc66703a57472825b8f7daa800
Philip <philiplofters@gmail.com>
2019-05-16 10:48:02 -0400

    adjusted top padding on paragraph in About section

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ const about = () => {
 		<div className="w-100 pt5 pb2" style={{backgroundColor: "#f7f9fb", color: "#3262A6"}}> 
 			<div className="w-90 center tc mb5" >
 				<h2 className='tc f3 lh-copy ttu fw5 tracked' style={{fontFamily: 'Helvetica, system sans-serif'}}>About Me</h2>
-				<p className="mv0 pv3" style={{fontFamily: 'Georgia, system serif', color: "#3262A6", lineHeight: "1.5em"}}>
+				<p className="mv0" style={{fontFamily: 'Georgia, system serif', color: "#3262A6", lineHeight: "1.5em"}}>
 					I've always had an interest in web development as someone who loves creation and design. 
 					I am a developer with close to three years of experience of developing and deploying projects to production.
 					Going through that time of practicing and learning has given me a flexible skillset. 
