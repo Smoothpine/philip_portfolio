791ae1ac776362e047e3f08bcb0699b5fc31c3b6
Philip <philiplofters@gmail.com>
2019-05-16 22:59:07 -0400

    Remove subtitles in skills to make it cleaner

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -15,9 +15,8 @@ import {ReactComponent as Git} from "./assets/git.svg";
 
 const skills = () => {
 	return(
-		<div className="w-100 pt5" style={{backgroundColor: "#32A0A6", color: "#f7f9fb", fontFamily: 'Helvetica, system sans-serif', paddingBottom: "2.4rem"}}>
+		<div className="w-100 pt5" style={{backgroundColor: "#32A0A6", color: "#f7f9fb", fontFamily: 'Helvetica, system sans-serif', paddingBottom: "2rem"}}>
 			<h2 className='tc f3 ttu fw5 lh-copy' style={{fontFamily: 'Helvetica, system sans-serif', letterSpacing: "0.03em"}}>Skills</h2>
-			<h1 className="f4 mt0 mb0 tc pv3 ph5 fw4" style={{fontFamily: 'Helvetica, system sans-serif'}}>Web Technologies</h1>
 			<div className="flex justify-center mt3">
 			  <div className="w-20 w-10-l pa3 mh1">
 			    <Html style={{fill: "#f7f9fb"}} />
@@ -46,7 +45,6 @@ const skills = () => {
 			    <h1 className="tc f6 f5-m lh-copy fw3" style={{marginTop: "-0.8em"}}>React</h1>
 			  </div>
 			</div>
-			<h1 className="f4 mt3 mb0 tc pv3 ph5 fw4" style={{fontFamily: 'Helvetica, system sans-serif'}}>Server & Databases</h1>
 			<div className="flex justify-center mt3">
 			  <div className="mw-20 w-20 w-10-l pa3 mh1">
 			    <Node style={{fill: "#f7f9fb"}} />
@@ -80,7 +78,6 @@ const skills = () => {
 			  </div>
 				*/}
 			</div>
-			<h1 className="f4 mt3 mb0 tc pv3 ph5 fw4" style={{fontFamily: 'Helvetica, system sans-serif'}}>Deployment</h1>
 			<div className="flex justify-center mt3">
 			  <div className="mw-20 w-20 w-10-l pa3 mh1">
 			    <GitHub style={{fill: "#f7f9fb"}} />
@@ -97,7 +94,6 @@ const skills = () => {
 			    <h1 className="tc f6 f5-m lh-copy fw3" style={{marginTop: "-0.8em"}}>Heroku</h1>
 			  </div>
 			</div>
-			<h1 className="f4 mt3 mb0 tc pv3 ph5 fw4" style={{fontFamily: 'Helvetica, system sans-serif'}}>Version Control</h1>
 			<div className="flex justify-center mt3">
 			  <div className="mw-20 w-20 w-10-l pa3 mh1">
 			    <Git style={{fill: "#f7f9fb"}} />
