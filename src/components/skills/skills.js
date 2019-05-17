8ff90abf38786fe1d24b46ed4f5db14212ea4716
Philip <philiplofters@gmail.com>
2019-05-16 22:31:27 -0400

    Added in more skills

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -8,14 +8,17 @@ import {ReactComponent as Node} from "./assets/nodejs.svg";
 import {ReactComponent as Express} from "./assets/express.svg";
 import {ReactComponent as Postgres} from "./assets/postgresql.svg";
 import {ReactComponent as MongoDB} from "./assets/mongodb.svg";
+import {ReactComponent as GitHub} from "./assets/github.svg";
+import {ReactComponent as Heroku} from "./assets/heroku.svg";
+import {ReactComponent as Git} from "./assets/git.svg";
 
 
 const skills = () => {
 	return(
-		<div className="w-100 pt5" style={{backgroundColor: "#32A0A6", color: "#f7f9fb", fontFamily: 'Helvetica, system sans-serif', paddingBottom: "2.45rem"}}>
+		<div className="w-100 pt5" style={{backgroundColor: "#32A0A6", color: "#f7f9fb", fontFamily: 'Helvetica, system sans-serif', paddingBottom: "2.4rem"}}>
 			<h2 className='tc f3 ttu fw5 lh-copy' style={{fontFamily: 'Helvetica, system sans-serif', letterSpacing: "0.03em"}}>Skills</h2>
 			<h1 className="f4 mt0 mb0 tc pv3 ph5 fw4" style={{fontFamily: 'Helvetica, system sans-serif'}}>Web Technologies</h1>
-			<div className="flex justify-center">
+			<div className="flex justify-center mt3">
 			  <div className="w-20 w-10-l pa3 mh1">
 			    <Html style={{fill: "#f7f9fb"}} />
 			  </div>
@@ -43,8 +46,8 @@ const skills = () => {
 			    <h1 className="tc f6 f5-m lh-copy fw3" style={{marginTop: "-0.8em"}}>React</h1>
 			  </div>
 			</div>
-			<h1 className="f4 mt4 mb0 tc pv3 ph5 fw4" style={{fontFamily: 'Helvetica, system sans-serif'}}>Server & Databases</h1>
-			<div className="flex justify-center">
+			<h1 className="f4 mt3 mb0 tc pv3 ph5 fw4" style={{fontFamily: 'Helvetica, system sans-serif'}}>Server & Databases</h1>
+			<div className="flex justify-center mt3">
 			  <div className="mw-20 w-20 w-10-l pa3 mh1">
 			    <Node style={{fill: "#f7f9fb"}} />
 			  </div>
@@ -58,7 +61,7 @@ const skills = () => {
 			    <MongoDB style={{fill: "#f7f9fb"}} />
 			  </div>		  
 			</div>
-			<div className="flex justify-center mb5">
+			<div className="flex justify-center">
 			  <div className="w-20 w-10-l mh1">
 			    <h1 className="tc f6 f5-m lh-copy fw3" style={{marginTop: "-0.8em"}}>Node</h1>
 			  </div>
@@ -76,8 +79,36 @@ const skills = () => {
 			    <h1 className="f5 lh-copy tc">Git</h1>
 			  </div>
 				*/}
-			</div>    
-		</div>
+			</div>
+			<h1 className="f4 mt3 mb0 tc pv3 ph5 fw4" style={{fontFamily: 'Helvetica, system sans-serif'}}>Deployment</h1>
+			<div className="flex justify-center mt3">
+			  <div className="mw-20 w-20 w-10-l pa3 mh1">
+			    <GitHub style={{fill: "#f7f9fb"}} />
+			  </div>
+			  <div className="mw-20 w-20 w-10-l pa3 mh1">
+			    <Heroku style={{fill: "#f7f9fb"}} />
+			  </div>
+			</div>		  
+			<div className="flex justify-center">
+			  <div className="w-20 w-10-l mh1">
+			    <h1 className="tc f6 f5-m lh-copy fw3" style={{marginTop: "-0.8em"}}>GitHub</h1>
+			  </div>
+			  <div className="w-20 w-10-l mh1">
+			    <h1 className="tc f6 f5-m lh-copy fw3" style={{marginTop: "-0.8em"}}>Heroku</h1>
+			  </div>
+			</div>
+			<h1 className="f4 mt3 mb0 tc pv3 ph5 fw4" style={{fontFamily: 'Helvetica, system sans-serif'}}>Version Control</h1>
+			<div className="flex justify-center mt3">
+			  <div className="mw-20 w-20 w-10-l pa3 mh1">
+			    <Git style={{fill: "#f7f9fb"}} />
+			  </div>
+			</div>		  
+			<div className="flex justify-center mb5">
+			  <div className="w-20 w-10-l mh1">
+			    <h1 className="tc f6 f5-m lh-copy fw3" style={{marginTop: "-0.8em"}}>Git</h1>
+			  </div>
+			</div>
+		</div>     
 	);
 }
 
